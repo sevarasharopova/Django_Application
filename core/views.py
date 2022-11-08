35310b081abd3ecbd8cbb20e66d6ad6708c9a5df
sevarasharopova <sevarasharopova23@gmail.com>
2022-11-08 12:34:43 +0500

    appli

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from frontend import *
+
+def gul1(request):
+    return render(request,'atirgul.html')
+
+def gul2(request):
+    return render(request,'liliya.html')
+
+def gul3(request):
+    return render(request,'lola.html')
+
+def gul4(request):
+    return render(request,'moychechak.html')
